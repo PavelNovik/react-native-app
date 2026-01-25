36b169b02250ba3762d2da39fbac3e0f28ae3e01
Pavel Novik <brn2befree@gmail.com>
2026-01-25 18:59:14 +0300

    change styles, add input

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,46 @@
-import { Text, View, StyleSheet } from "react-native";
+import {StyleSheet, Text, TextInput, View} from "react-native";
+import {useState} from "react";
 
 export default function Index() {
-  return (
-    <View
-      style={styles.container}
-    >
-      <Text>Something in the way!</Text>
-      <Text>What the fuck!</Text>
-      <Text>Prosze bardzo.</Text>
-    </View>
-  );
+    const [value, setValue] = useState<string>('');
+
+
+    return (
+        <View
+            style={styles.container}
+        >
+            <View>
+                <Text>Something in the way!</Text>
+                <TextInput style={[globalStyles.border, styles.input, {fontSize: 18,}]}
+                           placeholder={'Enter your name...'}
+                           placeholderTextColor={'black'}
+                           value={value}
+                           onChangeText={setValue}/>
+            </View>
+            <Text>What the fuck!</Text>
+            <Text>The man who sold the world</Text>
+        </View>
+    );
 }
 
-const styles= StyleSheet.create({
+const styles = StyleSheet.create({
     container: {
         flex: 1,
+        backgroundColor: '#f8ff6a',
         justifyContent: "center",
         alignItems: "center",
+    },
+    input: {
+        backgroundColor: 'white',
+        width: 200,
+        padding: 10,
+    }
+})
+
+const globalStyles = StyleSheet.create({
+    border: {
+        borderWidth: 2,
+        borderRadius: 5,
+        borderColor: '#ecad39',
     }
 })
