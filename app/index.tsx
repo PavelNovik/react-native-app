c273197d6526c53a17fe3010fd275e85317a99aa
Pavel Novik <brn2befree@gmail.com>
2026-01-26 15:59:46 +0300

    add hide keyboard component

--- app/index.tsx
+++ app/index.tsx
@@ -1,26 +1,55 @@
-import {StyleSheet, Text, TextInput, View} from "react-native";
-import {useState} from "react";
+import {Keyboard, StyleSheet, Text, TextInput, TouchableWithoutFeedback, View} from "react-native";
+import {ReactElement, ReactNode, useState} from "react";
+import {Checkbox} from "expo-checkbox";
+
+type Task = {
+    id: number;
+    title: string;
+    isDone: boolean;
+}
 
 export default function Index() {
     const [value, setValue] = useState<string>('');
+    const [tasks, setTasks] = useState<Task[]>([
+        {id: 1, title: 'HTML', isDone: true},
+        {id: 2, title: 'CSS', isDone: true},
+        {id: 3, title: 'JavaScript', isDone: false},
+        {id: 4, title: 'React', isDone: true},
+        {id: 5, title: 'React Native', isDone: false},
+    ]);
 
 
     return (
         <View
             style={styles.container}
         >
-            <View>
-                <Text>Something in the way!</Text>
-                <TextInput style={[globalStyles.border, styles.input, {fontSize: 18,}]}
-                           placeholder={'Enter your name...'}
-                           placeholderTextColor={'black'}
-                           value={value}
-                           onChangeText={setValue}/>
+
+            <Text>Something in the way!</Text>
+            <HideKeyboard>
+                <View style={[{width: '100%', alignItems: "center"}]}>
+                    <TextInput style={[globalStyles.border, styles.input, {fontSize: 18,}]}
+                               placeholder={'Enter your name...'}
+                               placeholderTextColor={'black'}
+                               value={value}
+                               onChangeText={setValue}/>
+                </View>
+            </HideKeyboard>
+            <View style={[{width: 200}]}>
+                {tasks.map((task: Task) => {
+                    return <View style={styles.section} key={task.id}>
+                        <Checkbox style={styles.checkbox} value={task.isDone}/>
+                        <Text>{task.title}</Text>
+                    </View>
+                })}
             </View>
             <Text>What the fuck!</Text>
             <Text>The man who sold the world</Text>
         </View>
     );
+}
+
+const HideKeyboard = ({children}: { children: ReactNode }): ReactElement => {
+    return <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>{children}</TouchableWithoutFeedback>
 }
 
 const styles = StyleSheet.create({
@@ -31,10 +60,17 @@ const styles = StyleSheet.create({
         alignItems: "center",
     },
     input: {
-        backgroundColor: 'white',
+        backgroundColor: '#f3bf98',
         width: 200,
         padding: 10,
-    }
+    },
+    section: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    checkbox: {
+        margin: 8,
+    },
 })
 
 const globalStyles = StyleSheet.create({
