a46deffeb1839809ffa8693efacc29438adf1b1e
Pavel Novik <brn2befree@gmail.com>
2026-01-27 22:22:21 +0300

    add task api

---
+++ app/api/taskApi.ts
@@ -0,0 +1,21 @@
+import {DomainTask, GetTasksResponse, UpdateTaskModel} from "@/app/types/task-models";
+import {BaseResponse} from "@/app/types/models";
+import {instance} from "@/app/api/instance";
+
+export const tasksApi = {
+    getTasks(todolistId: string) {
+        return instance.get<GetTasksResponse>(`/todo-lists/${todolistId}/tasks`)
+    },
+    createTask(payload: { todolistId: string; title: string }) {
+        const {todolistId, title} = payload
+        return instance.post<BaseResponse<{ item: DomainTask }>>(`/todo-lists/${todolistId}/tasks`, {title})
+    },
+    updateTask(payload: { todolistId: string; taskId: string; model: UpdateTaskModel }) {
+        const {todolistId, taskId, model} = payload
+        return instance.put<BaseResponse<{ item: DomainTask }>>(`/todo-lists/${todolistId}/tasks/${taskId}`, model)
+    },
+    deleteTask(payload: { todolistId: string; taskId: string }) {
+        const {todolistId, taskId} = payload
+        return instance.delete<BaseResponse>(`/todo-lists/${todolistId}/tasks/${taskId}`)
+    },
+}
