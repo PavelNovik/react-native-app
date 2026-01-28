378f533eb5c88fcbfdc5222b52509e936ca32ab6
Pavel Novik <brn2befree@gmail.com>
2026-01-29 01:05:08 +0300

    configure instance

--- app/api/instance.ts
+++ app/api/instance.ts
@@ -1,9 +1,10 @@
 import axios from "axios"
 
 export const instance = axios.create({
-    baseURL: 'import.meta.env.VITE_BASE_URL',
+    baseURL: 'https://social-network.samuraijs.com/api/1.1/',
+    withCredentials: true,
     headers: {
-        Authorization: `Bearer `,
-        "API-KEY": 'import.meta.env.VITE_API_KEY',
+        // Authorization: `Bearer `,
+        "API-KEY": '58d16126-c44d-4aae-84b6-9bdc00838cf2',
     },
 })
