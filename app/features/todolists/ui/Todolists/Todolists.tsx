0903f7ecb54a66bb54b27dfaab9d793f97df10f6
Pavel Novik <brn2befree@gmail.com>
2026-01-29 20:31:35 +0300

    create todolists

---
+++ app/features/todolists/ui/Todolists/Todolists.tsx
@@ -0,0 +1,28 @@
+import {fetchTodolistsTC, selectTodolists} from "@/app/store/todolist-slice";
+import {useAppSelector} from "@/app/hooks/useAppSelector";
+import {useAppDispatch} from "@/app/hooks/useAppDispatch";
+import {useEffect} from "react";
+import {View} from "react-native";
+import {TodolistItem} from "@/app/features/todolists/ui/Todolists/TodolistItem/TodolistItem";
+
+export const Todolists = () => {
+    const todolists = useAppSelector(selectTodolists)
+
+    const dispatch = useAppDispatch()
+
+    useEffect(() => {
+        dispatch(fetchTodolistsTC())
+    }, [])
+
+    return (
+        <>
+            {todolists.map((todolist) => (
+                <View key={todolist.id}>
+                    <View>
+                        <TodolistItem todolist={todolist}/>
+                    </View>
+                </View>
+            ))}
+        </>
+    )
+}
