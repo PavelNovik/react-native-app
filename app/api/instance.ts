65fd85cc6d8a0ccf76675208c678b222223102c1
Pavel Novik <brn2befree@gmail.com>
2026-01-30 00:31:57 +0300

    change instance

--- app/api/instance.ts
+++ app/api/instance.ts
@@ -1,12 +1,13 @@
 import axios from "axios"
+import {BASE_URL} from "@/app/api/baseURL";
 
 export const instance = axios.create({
-    baseURL: "/samurai/api/1.1",
+    baseURL: BASE_URL,
     withCredentials: true,
     headers: {
         Authorization: `Bearer ${process.env.EXPO_PUBLIC_AUTH_TOKEN!}`,
         "API-KEY": process.env.EXPO_PUBLIC_API_KEY!,
-        
+
     },
 })
 // import {BASE_URL} from "./baseURL"
