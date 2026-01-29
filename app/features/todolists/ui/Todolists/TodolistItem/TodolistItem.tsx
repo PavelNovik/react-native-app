5dd7a9c0663476a073d3e451d85863224e1b8b87
Pavel Novik <brn2befree@gmail.com>
2026-01-29 20:31:44 +0300

    create todolistItem

---
+++ app/features/todolists/ui/Todolists/TodolistItem/TodolistItem.tsx
@@ -0,0 +1,36 @@
+import {DomainTodolist} from "@/app/store/todolist-slice";
+import {useAppDispatch} from "@/app/hooks/useAppDispatch";
+import {createTaskTC} from "@/app/store/tasks-slice";
+import {Text, View} from "react-native";
+import {Tasks} from "@/app/features/todolists/ui/Todolists/TodolistItem/Tasks/Tasks";
+
+type Props = {
+    todolist: DomainTodolist
+}
+
+export const TodolistItem = ({todolist}: Props) => {
+    const dispatch = useAppDispatch()
+
+    const createTask = (title: string) => {
+        dispatch(createTaskTC({todolistId: todolist.id, title}))
+    }
+
+    return (
+        <div>
+            {/*<TodolistTitle todolist={todolist}/>*/}
+            <View>
+                <Text>Todo title</Text>
+            </View>
+            {/*<CreateItemForm onCreateItem={createTask} disabled={todolist.entityStatus === "loading"}/>*/}
+            <View>
+                <Text>CreateItemForm</Text>
+            </View>
+            <Tasks todolist={todolist}/>
+
+            <View>
+                <Text>FilterButtons</Text>
+            </View>
+            {/*<FilterButtons todolist={todolist}/>*/}
+        </div>
+    )
+}
