ca4a85e3000236515eeaff73ca88465b1487eb33
Pavel Novik <brn2befree@gmail.com>
2026-02-03 15:38:15 +0300

    change View to ScrollView

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,54 @@
-import {View} from "react-native";
-import TodoOld from "@/app/todo/todo";
+import React from "react";
+import {ScrollView, StyleSheet} from "react-native";
 import {Todolists} from "@/app/features/todolists/ui/Todolists/Todolists";
 
 export default function Index() {
 
 
     return (
-        <View style={[{flex: 1}]}>
-            <TodoOld/>
+        <ScrollView style={[styles.container]}
+                    contentContainerStyle={[styles.content]}>
+            {/*<TodoOld/>*/}
             <Todolists/>
-        </View>
+        </ScrollView>
 
     );
 }
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#0f0e17',
+
+    },
+    content: {
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    text: {color: '#fffffe'},
+    input: {
+        backgroundColor: '#f3bf98',
+        width: 200,
+        padding: 10,
+    },
+    action: {
+        margin: 5
+    },
+    section: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginBottom: 3
+    },
+    checkbox: {
+        margin: 8,
+    },
+})
+
+const globalStyles = StyleSheet.create({
+    border: {
+        borderWidth: 2,
+        borderRadius: 5,
+        borderColor: '#ecad39',
+    }
+})
