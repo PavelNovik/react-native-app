28d7b65735a2c48fd221dbbc900155ec7195ab7a
Pavel Novik <brn2befree@gmail.com>
2026-02-04 00:37:10 +0300

    add todolist title component

---
+++ app/features/todolists/ui/Todolists/TodolistItem/TodolistTitle/TodolistTitle.tsx
@@ -0,0 +1,38 @@
+import {changeTodolistTitleTC, deleteTodolistTC, DomainTodolist} from "@/app/store/todolist-slice";
+import {useAppDispatch} from "@/app/hooks/useAppDispatch";
+import {TouchableOpacity, View} from "react-native";
+import {EditableSpan} from "@/app/components/EditableSpan/EditableSpan";
+import {Ionicons} from "@expo/vector-icons";
+import React from "react";
+
+type Props = {
+    todolist: DomainTodolist
+}
+
+export const TodolistTitle = ({todolist}: Props) => {
+    const {id, title, entityStatus} = todolist
+
+    const dispatch = useAppDispatch()
+
+    const deleteTodolist = () => {
+        dispatch(deleteTodolistTC(id))
+    }
+
+    const changeTodolistTitle = (title: string) => {
+        dispatch(changeTodolistTitleTC({id, title}))
+    }
+
+    return (
+        <View>
+            <View>
+                <EditableSpan value={title} onChange={changeTodolistTitle}/>
+            </View>
+            <TouchableOpacity>
+                <Ionicons name="trash-sharp" size={24} color="white" onPress={deleteTodolist}/>
+            </TouchableOpacity>
+            {/*<IconButton onClick={deleteTodolist} disabled={entityStatus === "loading"}>*/}
+            {/*    <DeleteIcon/>*/}
+            {/*</IconButton>*/}
+        </View>
+    )
+}
