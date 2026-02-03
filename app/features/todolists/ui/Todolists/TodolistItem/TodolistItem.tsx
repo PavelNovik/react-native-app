36081972bb8517ef4d58accae44a7e5673667b55
Pavel Novik <brn2befree@gmail.com>
2026-02-03 23:16:39 +0300

    add some changes in styles

--- app/features/todolists/ui/Todolists/TodolistItem/TodolistItem.tsx
+++ app/features/todolists/ui/Todolists/TodolistItem/TodolistItem.tsx
@@ -4,6 +4,7 @@ import {createTaskTC} from "@/app/store/tasks-slice";
 import {StyleSheet, Text, View} from "react-native";
 import {Tasks} from "@/app/features/todolists/ui/Todolists/TodolistItem/Tasks/Tasks";
 import React from "react";
+import {CreateItemForm} from "@/app/components/CreateItemForm/CreateItemForm";
 
 type Props = {
     todolist: DomainTodolist
@@ -19,17 +20,15 @@ export const TodolistItem = ({todolist}: Props) => {
     return (
         <View>
             {/*<TodolistTitle todolist={todolist}/>*/}
-            <View>
+            <View style={[{marginVertical: 10}]}>
                 <Text style={[styles.text]}>{todolist.title}</Text>
             </View>
-            {/*<CreateItemForm onCreateItem={createTask} disabled={todolist.entityStatus === "loading"}/>*/}
-            <View>
-                <Text style={[styles.text]}>CreateItemForm</Text>
-            </View>
+            <CreateItemForm onCreateItem={createTask} /* disabled={todolist.entityStatus === "loading"} *//>
+
             <Tasks todolist={todolist}/>
 
             <View>
-                <Text style={[styles.text]}>FilterButtons</Text>
+                <Text style={[styles.description]}>FilterButtons</Text>
             </View>
             {/*<FilterButtons todolist={todolist}/>*/}
         </View>
@@ -37,5 +36,11 @@ export const TodolistItem = ({todolist}: Props) => {
 }
 
 const styles = StyleSheet.create({
-    text: {color: '#fffffe'},
+    text: {
+        color: '#fffffe',
+        fontSize: 18,
+    },
+    description: {
+        color: '#c7c725',
+    }
 })
