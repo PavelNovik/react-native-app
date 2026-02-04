252ce8944d842cf69b24dd26a47f859be9559c35
Pavel Novik <brn2befree@gmail.com>
2026-02-04 23:15:00 +0300

    add navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,17 @@ import {store} from "@/app/store/store";
 
 export default function RootLayout() {
     return <Provider store={store}>
-        <Stack screenOptions={{headerShown: false}}/>
+        <Stack
+            screenOptions={{
+                headerShown: false,
+                animation: "slide_from_right"
+            }}
+        >
+            {/*<Stack.Screen*/}
+            {/*    name="index"*/}
+            {/*    options={{title: "Todolists"}}*/}
+            {/*    */}
+            {/*/>*/}
+        </Stack>
     </Provider>;
 }
