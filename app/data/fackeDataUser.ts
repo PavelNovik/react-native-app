1eead712dc9ff9a48b774d6eb17cd1473dd38e86
Pavel Novik <brn2befree@gmail.com>
2026-02-06 00:53:02 +0300

    add data for homework

---
+++ app/data/fackeDataUser.ts
@@ -0,0 +1,25 @@
+export type FakeDataUserType = {
+    id: number;
+    firstName: string;
+    lastName: string;
+    avatar: string | null;
+    location: {
+        country: string;
+        city: string;
+    };
+    skills: string[];
+    role: string;
+}
+
+export const fakeDataUser: FakeDataUserType[] = [{
+    id: 1,
+    firstName: 'John',
+    lastName: 'Doe',
+    avatar: 'https://icons-for-free.com/iconfiles/png/512/avatar-1320568024619304547.png',
+    location: {
+        country: 'Spain',
+        city: 'Barcelona'
+    },
+    skills: ['HTML', 'CSS', 'JS', 'React', 'Redux', 'Next JS'],
+    role: 'Boss'
+}]
