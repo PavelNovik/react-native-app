fd0329062bcb0f4d6c46de7a8fe2310ffb1a6667
Pavel Novik <brn2befree@gmail.com>
2026-02-06 00:37:47 +0300

    add safe area provider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,31 @@
 import {Stack} from "expo-router";
 import {Provider} from "react-redux";
 import {store} from "@/app/store/store";
+import {SafeAreaProvider} from "react-native-safe-area-context";
 
 export default function RootLayout() {
     return <Provider store={store}>
-        <Stack
-            screenOptions={{
-                headerShown: false,
-                animation: "slide_from_right"
-            }}
-        >
-            {/*<Stack.Screen*/}
-            {/*    name="index"*/}
-            {/*    options={{title: "Todolists"}}*/}
-            {/*    */}
-            {/*/>*/}
-        </Stack>
+        <SafeAreaProvider>
+            <Stack
+                screenOptions={{
+                    headerShown: false,
+                    animation: "slide_from_right"
+                }}
+            >
+                {/*<Stack.Screen*/}
+                {/*    name="index"*/}
+                {/*    options={{title: "Todolists"}}*/}
+                {/*    */}
+                {/*/>*/}
+                <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
+                <Stack.Screen
+                    name="modal"
+                    options={{
+                        presentation: "modal",
+                        title: "Modal",
+                    }}
+                />
+            </Stack>
+        </SafeAreaProvider>
     </Provider>;
 }
