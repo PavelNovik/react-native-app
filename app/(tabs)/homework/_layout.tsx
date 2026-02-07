073750c569dcf80316997628cb1dbbd44e875c4c
Pavel Novik <brn2befree@gmail.com>
2026-02-07 21:43:06 +0300

    add screen options

--- app/(tabs)/homework/_layout.tsx
+++ app/(tabs)/homework/_layout.tsx
@@ -1,5 +1,13 @@
 import {Stack} from "expo-router";
 
 export default function HomeworkLayout() {
-    return <Stack screenOptions={{title: 'Users'}}/>;
+    return <Stack screenOptions={{
+        title: 'Users', headerStyle: {
+            backgroundColor: '#f4511e',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+            fontWeight: 'bold',
+        },
+    }}/>;
 }
