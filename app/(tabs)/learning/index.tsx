b887be39ee29251a7888fe23805181cb06cadcc2
Pavel Novik <brn2befree@gmail.com>
2026-02-07 20:31:13 +0300

    add some changes

--- app/(tabs)/learning/index.tsx
+++ app/(tabs)/learning/index.tsx
@@ -1,12 +1,17 @@
-import {Pressable, Text} from "react-native";
+import {Pressable, Text, View} from "react-native";
 import {useRouter} from "expo-router";
+import {MaterialCommunityIcons} from "@expo/vector-icons";
 
 export default function Learning() {
     const router = useRouter();
 
     return (
         <Pressable onPress={() => router.push("/learning/details")}>
-            <Text>Open details</Text>
+            <View style={[{flexDirection: 'row'}]}>
+                <MaterialCommunityIcons name="details" size={24} color="black"/>
+                <Text>Open details</Text>
+            </View>
+
         </Pressable>
     );
 }
