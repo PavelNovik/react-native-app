f9b8b65312495df3d706ae12504aee45f2f167e8
Pavel Novik <brn2befree@gmail.com>
2026-02-07 21:43:18 +0300

    add some users in data

--- app/data/fackeDataUser.ts
+++ app/data/fackeDataUser.ts
@@ -22,4 +22,137 @@ export const fakeDataUser: FakeDataUserType[] = [{
     },
     skills: ['HTML', 'CSS', 'JS', 'React', 'Redux', 'Next JS'],
     role: 'Boss'
-}]
+},
+    {
+        id: 2,
+        firstName: 'Vik',
+        lastName: 'Barrel',
+        avatar: 'https://icons-for-free.com/iff/png/256/avatar+user+account+confirm+approve+complete-1320567854562615796.png',
+        location: {
+            country: 'Spain',
+            city: 'Barcelona'
+        },
+        skills: ['HTML', 'CSS', 'JS', 'React', 'Redux', 'Next JS'],
+        role: 'Boss'
+    },
+    {
+        id: 3,
+        firstName: 'Dave',
+        lastName: 'Growl',
+        avatar: 'https://icons-for-free.com/iff/png/256/avatar+man+people+person+profile+user+icon-1320195822522856427.png',
+        location: {
+            country: 'Spain',
+            city: 'Barcelona'
+        },
+        skills: ['HTML', 'CSS', 'JS', 'React', 'Redux', 'Next JS'],
+        role: 'Boss'
+    },
+    {
+        id: 4,
+        firstName: 'Dan',
+        lastName: 'Miller',
+        avatar: 'https://icons-for-free.com/iff/png/256/avatar+emoji+emoticon+man+smiley+surprised+icon-1320184903641701003.png',
+        location: {
+            country: 'Spain',
+            city: 'Barcelona'
+        },
+        skills: ['HTML', 'CSS', 'JS', 'React', 'Redux', 'Next JS'],
+        role: 'Boss'
+    },
+    {
+        id: 5,
+        firstName: 'Washington',
+        lastName: 'Lee',
+        avatar: 'https://icons-for-free.com/iff/png/256/avatar+conversation+troll+icon-1320183939390015504.png',
+        location: {
+            country: 'Spain',
+            city: 'Barcelona'
+        },
+        skills: ['HTML', 'CSS', 'JS', 'React', 'Redux', 'Next JS'],
+        role: 'Boss'
+    },
+    {
+        id: 6,
+        firstName: 'Bruce',
+        lastName: 'Willey',
+        avatar: 'https://icons-for-free.com/iff/png/256/avatar+default+head+person+unknown+user+icon-1320196264066691417.png',
+        location: {
+            country: 'Spain',
+            city: 'Barcelona'
+        },
+        skills: ['HTML', 'CSS', 'JS', 'React', 'Redux', 'Next JS'],
+        role: 'Boss'
+    },
+    {
+        id: 7,
+        firstName: 'Caper',
+        lastName: 'Matter',
+        avatar: 'https://icons-for-free.com/iff/png/256/icon-1320184415870874938.png',
+        location: {
+            country: 'Spain',
+            city: 'Barcelona'
+        },
+        skills: ['HTML', 'CSS', 'JS', 'React', 'Redux', 'Next JS'],
+        role: 'Boss'
+    },
+    {
+        id: 8,
+        firstName: 'John',
+        lastName: 'Doe',
+        avatar: 'https://icons-for-free.com/iff/png/256/avatar-1320568024619304547.png',
+        location: {
+            country: 'Spain',
+            city: 'Barcelona'
+        },
+        skills: ['HTML', 'CSS', 'JS', 'React', 'Redux', 'Next JS'],
+        role: 'Boss'
+    },
+    {
+        id: 9,
+        firstName: 'John',
+        lastName: 'Doe',
+        avatar: 'https://icons-for-free.com/iff/png/256/avatar-1320568024619304547.png',
+        location: {
+            country: 'Spain',
+            city: 'Barcelona'
+        },
+        skills: ['HTML', 'CSS', 'JS', 'React', 'Redux', 'Next JS'],
+        role: 'Boss'
+    },
+    {
+        id: 10,
+        firstName: 'John',
+        lastName: 'Doe',
+        avatar: 'https://icons-for-free.com/iff/png/256/avatar-1320568024619304547.png',
+        location: {
+            country: 'Spain',
+            city: 'Barcelona'
+        },
+        skills: ['HTML', 'CSS', 'JS', 'React', 'Redux', 'Next JS'],
+        role: 'Boss'
+    }
+    , {
+        id: 11,
+        firstName: 'John',
+        lastName: 'Doe',
+        avatar: 'https://icons-for-free.com/iff/png/256/avatar-1320568024619304547.png',
+        location: {
+            country: 'Spain',
+            city: 'Barcelona'
+        },
+        skills: ['HTML', 'CSS', 'JS', 'React', 'Redux', 'Next JS'],
+        role: 'Boss'
+    },
+    {
+        id: 12,
+        firstName: 'John',
+        lastName: 'Doe',
+        avatar: 'https://icons-for-free.com/iff/png/256/avatar-1320568024619304547.png',
+        location: {
+            country: 'Spain',
+            city: 'Barcelona'
+        },
+        skills: ['HTML', 'CSS', 'JS', 'React', 'Redux', 'Next JS'],
+        role: 'Boss'
+    }
+]
